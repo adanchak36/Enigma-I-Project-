174a56e745dee31ddfee2df33247c02f70a4c56b
Alexander Danchak <alexander.danchak00@gmail.com>
2018-12-25 16:31:09 -0500

    Update rotor_setup.h

--- rotor_setup.h
+++ rotor_setup.h
@@ -1,8 +1,10 @@
 #include "machine_struct.h"
 void getMachineInfo(struct machine *machine1);
 
-void setup_Machine(char left_rotor[], char mid_rotor[], char right_rotor[], struct machine *machine1);
+void setup_Machine(char reflector[], char left_rotor[], char mid_rotor[], char right_rotor[], struct machine *machine1);
 
 void setup_rotors(char left_rotor[], char mid_rotor[], char right_rotor[], struct machine *machine1);
 
 void ringSet(char left_rotor[], char mid_rotor[], char right_rotor[], struct machine *machine1); //
+
+//struct create 
