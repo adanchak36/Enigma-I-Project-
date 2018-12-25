cb568fbd58c9c8f691f4a7fb7f8d45fac1ad63bc
Alexander Danchak <alexander.danchak00@gmail.com>
2018-12-25 16:31:51 -0500

    Update enigma_encode.h

--- enigma_encode.h
+++ enigma_encode.h
@@ -2,8 +2,11 @@
 #include "rotor_setup.h"
 #include "machine_struct.h"
 
-char input_enigma(struct machine *machine1); //input->scramble plugboard
+char getChar(void);
 
+void encryptPlugBoard(char *plainC, struct machine *machine1);
+
+char encryptRotors(char plainC, struct machine *machine1, char right_rotor[], char mid_rotor[], char left_rotor[], char reflector[]);
 
 
 
