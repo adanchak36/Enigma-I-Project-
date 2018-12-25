787c2814e77c895d8f8f61175620ac427287ec97
Alexander Danchak <alexander.danchak00@gmail.com>
2018-12-25 16:30:43 -0500

    Update machine_struct.h

--- machine_struct.h
+++ machine_struct.h
@@ -4,6 +4,7 @@
 typedef struct machine{
     int rotor_order[3];//(1,2,3)
     int rotor_sett[3];// (03,18,14)
+    char reflector; //(B) 
     char plug_board1[10]; //input
     char plug_board2[10]; //scramble
 } machine;
